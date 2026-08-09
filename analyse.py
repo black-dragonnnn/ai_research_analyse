7b5c91b30c84b411b23a5b20599c45ddbfb74734
black-dragonnnn <167535611+black-dragonnnn@users.noreply.github.com>
2026-08-09 16:06:27 +0800

    Integrate MySQL for data storage and retrieval
    Added MySQL integration for cleaned data and statistics.

--- analyse.py
+++ analyse.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import re
+from sqlalchemy import create_engine, text
 from collections import Counter
+from utils import classify_organization, clean_title
+import config
 
 plt.rcParams["font.sans-serif"] = ["SimHei"]  # 解决中文乱码
 plt.rcParams["axes.unicode_minus"] = False
@@ -19,10 +21,25 @@ data_new["Affiliation"] = data_new["Affiliation"].str.strip()
 data_new = data_new[data_new["Affiliation"].notna()]
 print(f"清洗后有效数据:{len(data_new)}")
 
+# 写入mysql
+engine = create_engine("mysql+pymysql://root:123777@localhost:3306/paper_analysis?charset=utf8mb4")
+data_new.to_sql(
+    name="papers_clean",   
+    con=engine,           
+    if_exists="replace",    
+    index=False             # 不要把pandas自动索引存进数据库
+)
 
 # --- 统计每年的论文数量 ---
 year_count = data_new["Year"].value_counts().sort_index()
 print(year_count)
+#写入mysql
+year_count.to_sql(
+    name="papers_year_count",    # MySQL里的表名
+    con=engine,            
+    if_exists="replace",   
+    index=True          # 不要把pandas自动索引存进数据库
+)
 print(type(year_count))
 plt.figure(figsize=(10,5)) 
 plt.plot(year_count.index, year_count.values)
@@ -32,26 +49,25 @@ plt.title("三大会议历年论文数量")
 plt.xticks(range(int(year_count.index.min()), int(year_count.index.max()) + 1))
 ax = plt.gca()
 ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, pos: str(int(x))))
+plt.savefig(fname="picture/历年论文数量.png", dpi=config.DPI, bbox_inches=None)
 plt.show()  
 
-# --- 历年高校与科技企业论文产出对比 ---
-def classify_organization(org_name):
-    org_name = str(org_name).lower()
-    company_keywords = ["google", "microsoft", "meta", "deepmind", "amazon", "baidu", "tencent"]
-    if any(k in org_name for k in company_keywords):
-        return "大型科技企业"
-    elif any(word in org_name for word in ["university", "institute", "college"]):
-        return "高校/科研院所"
-    else:
-        return "其他机构"
 
+# --- 历年高校与科技企业论文产出对比 ---
 # 新增分类标签
 data_new["机构类型"] = data_new["Affiliation"].apply(classify_organization)
 
 # 按年份 + 机构类型聚合统计
 year_Affi_df = data_new.groupby(["Year", "机构类型"]).size().unstack()
 print(year_Affi_df)
-
+#写入mysql
+year_Affi_df.to_sql(
+    name="papers_year_Affi",   
+    con=engine,            
+    if_exists="replace",   
+    index=True        
+)
+print(type(year_Affi_df))
 plt.figure(figsize=(10,5))
 year_Affi_df.plot(ax=plt.gca())
 plt.xlabel("年份")
@@ -63,31 +79,10 @@ plt.legend()
 plt.xticks(range(int(year_Affi_df.index.min()), int(year_Affi_df.index.max()) + 1))
 ax = plt.gca()
 ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, pos: str(int(x))))
+plt.savefig(fname="picture/机构产出对比.png", dpi=config.DPI, bbox_inches=None)
 plt.show()
 
-
-
 # --- 统计热词 ---
-# 停用词（过滤无意义虚词，可自行扩充）
-stop_words = {
-    'the', 'of', 'and', 'a', 'in', 'for', 'on', 'with', 'to', 'is', 'are', 
-    'an', 'by', 'from', 'as', 'at', 'be', 'this', 'that', 'it', 'its',
-    'we', 'our', 'can', 'has', 'have', 'not', 'or', 'but', 'than', 'more',
-    'based', 'using', 'via', 'towards', 'into', 'over', 'under', 'new',
-    'toward', 'about', 'between', 'among', 'which', 'all', 'each', 'some','learning'
-}
-
-def clean_title(Title):
-    """清洗标题：小写、清除符号、分词、过滤停用词"""
-    Title = str(Title).lower()
-    # 匹配所有非小写字母,非空格的字符，替换为空格
-    Title = re.sub(r'[^a-z\s]', ' ', Title)
-    words = Title.split()
-    # 过滤停用词、长度小于3的无意义短单词
-    valid_words = [w for w in words if w not in stop_words and len(w) >= 3]
-    return valid_words
-
-# 统计热词
 all_words = []
 for Title_text in data_new["Title"]:
     all_words.extend(clean_title(Title_text))
@@ -100,7 +95,15 @@ top_words = word_counter.most_common(top_num)
 print(f"标题热词 Top {top_num}：")
 for word, count in top_words:
     print(f"{word} : {count}")
-
+#写入mysql
+top_words_df = pd.DataFrame(top_words, columns=["热词", "出现频次"])
+top_words_df.to_sql(
+    name="papers_top_words",   
+    con=engine,            
+    if_exists="replace",   
+    index=True        
+)
+print(type(top_words_df))
 # 绘制横向柱状图 
 words_index = [item[0] for item in top_words]
 count_index = [item[1] for item in top_words]
@@ -112,4 +115,5 @@ plt.xlabel("出现频次")
 plt.ylabel("热词")
 plt.title(f"论文标题 Top {top_num} 高频热词统计")
 plt.tight_layout()
+plt.savefig(fname="picture/top20热词.png", dpi=config.DPI, bbox_inches=None)
 plt.show()  
